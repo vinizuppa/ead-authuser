2973b1789aae7fc00b5042cccdcd07063efce2fd
vinizuppa <vcordeiro12@gmail.com>
2022-04-25 21:03:01 -0300

    Implementado Retry Resilience4j e Configurado Timeout

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,6 +3,7 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,6 +32,7 @@ public class CourseClient {
     @Value("${ead.api.url.course}")//Capta a URL definida no application.yaml
     String REQUEST_URL_COURSE;
 
+    //@Retry(name = "retryInstance")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
         List<CourseDto> searchResult = null;
         String url =  REQUEST_URL_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable);//Chama serviço que cria a URL.
--- src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
+++ src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
@@ -6,11 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestTemplateConfig {//Classe de configuração
+
+    static final int TIMEOUT = 5000;
     @LoadBalanced //Define para utilizar balanceamento de carga do microservice course.
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder){//Metodo produtor que retorna um RestTemplate.
-        return builder.build();
+        return builder
+                .setConnectTimeout(Duration.ofMillis(TIMEOUT))//Tempo limite de conexão
+                .setReadTimeout(Duration.ofMillis(TIMEOUT))//Tempo limite de leitura
+                .build();
     }
 }
